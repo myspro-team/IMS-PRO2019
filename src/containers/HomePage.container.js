9aa526a0bcafc3d110afd30841bd078d424e9407
thonghv <thonghv.pine@gmail.com>
2019-07-21 18:03:44 +0700

    init resource

---
+++ src/containers/HomePage.container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+      <div className="page-container">
+        <div className="">
+          Welcome to ABS
+        </div>
+      </div>
+  );                                  
+};
+   
+export default HomePage;
+          
