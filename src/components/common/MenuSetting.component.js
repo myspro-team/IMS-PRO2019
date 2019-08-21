764a61882637c41a658624a17becbd5397cdc040
nhanhaloli <thanhnhanpro.pro@gmail.com>
2019-08-21 09:25:04 +0700

    hoan_thien_menu

--- src/components/common/MenuSetting.component.js
+++ src/components/common/MenuSetting.component.js
@@ -15,8 +15,8 @@ const menuSeting = [
         icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z',
         url: '/internship'
     },
-    { menu: 'Internship', icon: '', url: '/' },
-    { menu: 'Import', icon: '', url: '/' },
+    { menu: 'Internship', icon: 'M19 13H5v-2h14v2z', url: '/' },
+    { menu: 'Import File', icon: 'M19 13H5v-2h14v2z', url: '/' },
     {
         menu: 'Courses',
         icon: 'M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 4h5v8l-2.5-1.5L6 12V4z',
@@ -81,7 +81,7 @@ class MenuSettingComponent extends Component {
                                     <path d={data.icon} />
                                 </SvgIcon>
                             </ListItemIcon>
-                            <ListItemText primary={data.menu} />                        
+                            <ListItemText className='ml-x' primary={data.menu} />                        
                         </ListItem>
                     </div>
                     </div>
