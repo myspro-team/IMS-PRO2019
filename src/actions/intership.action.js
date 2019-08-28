d4ffcdf3f34afbe9ea294089352eb9ab10c743dd
nhanhaloli <thanhnhanpro.pro@gmail.com>
2019-08-28 08:44:41 +0700

    update import function

--- src/actions/intership.action.js
+++ src/actions/intership.action.js
@@ -89,14 +89,26 @@ export const updateIntern = (data, id) => {
 
 export const deleteIntern = (id, data) => {
     return function(dispatch) {
-        IntershipService.deleteIntern(function(error, result, status, xhr) {
+        IntershipService.deleteIntern(function(error, resultAdd, status, xhr) {
             if(error){
                 console.log('Delete error: ....');
                 return;
             }
-            console.log(result)
+            console.log(resultAdd)
             return dispatch({type: types.DELETE_INTERN, oldIntern:result})
         },id, data)
+    }
+}
+
+export const addDataToApi = () => {
+    return function(dispatch) {
+        IntershipService.addIntern(function(error, result, status, xhr) {
+            if(error){
+                console.log('co loi');
+                return;
+            }
+            return dispatch({type: types.ADD_DATA_TO_API}, newIntern:resultAdd)
+        },data)
     }
 }
 
--- src/core/common/action.types.js
+++ src/core/common/action.types.js
@@ -9,6 +9,8 @@ export const UPDATE_INTERN = 'UPDATE_INTERN';
 
 export const DELETE_INTERN = 'DELETE_INTERN';
 
+export const ADD_DATA_TO_API = 'ADD_DATA_TO_API'
+
 export const GET_TOEIC_SCHEDULE_LIST = 'GET_TOEIC_SCHEDULE_LIST';
 
 export const UPLOAD_FILES = 'UPLOAD_FILES';
