5779473949d4fd68053aceace1dd5f363db5758d
PhanHoai <pthoai-batch2@sdc.edu.vn>
2019-08-29 15:30:43 +0700

    'List_schedule_toeic

--- src/containers/Login/Home.container.js
+++ src/containers/Login/Home.container.js
@@ -1,62 +1,62 @@
-import Login from "./Login.container";
-import React, { Component } from 'react';
-import "./../../App.css"
-import HomePageContainer from "./HomePage.container";
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user : JSON.parse(sessionStorage.getItem('user')),//lay data sau khi dang nhap
-      name:{}
-    }
-    // console.log(this.state.user)
-  };
-  onLogin (value) {
-    console.log(value)
-    if (value !==null) {
-      this.setState({
-        user:value
-      });
-    }
-    // localStorage.setItem('user',JSON.stringify(this.state.user));//lưu user lên local storage
-  }
-  onLogout(){
-    this.setState({
-      user : null
-    });
-    sessionStorage.clear();
-  }
-  handleComponent () {
-    if (this.state.user === null) {
-      return (
-      <div className="flexible-content">
-      <main id="content" className="p-5">
-      <Login onLogin = {this.onLogin.bind(this)}></Login>
-      </main>
-      </div>
-      )
-    }
-    else{
-      return (
-        <div className="flexible-content">
-            <HomePageContainer />
-        </div>
-      )
-    }
+// import Login from "./Login.container";
+// import React, { Component } from 'react';
+// import "./../../App.css"
+// import HomePageContainer from "./HomePage.container";
+// class HomePage extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       user : JSON.parse(sessionStorage.getItem('user')),//lay data sau khi dang nhap
+//       name:{}
+//     }
+//     // console.log(this.state.user)
+//   };
+//   onLogin (value) {
+//     console.log(value)
+//     if (value !==null) {
+//       this.setState({
+//         user:value
+//       });
+//     }
+//     // localStorage.setItem('user',JSON.stringify(this.state.user));//lưu user lên local storage
+//   }
+//   onLogout(){
+//     this.setState({
+//       user : null
+//     });
+//     sessionStorage.clear();
+//   }
+//   handleComponent () {
+//     if (this.state.user === null) {
+//       return (
+//       <div className="flexible-content">
+//       <main id="content" className="p-5">
+//       <Login onLogin = {this.onLogin.bind(this)}></Login>
+//       </main>
+//       </div>
+//       )
+//     }
+//     else{
+//       return (
+//         <div className="flexible-content">
+//             <HomePageContainer />
+//         </div>
+//       )
+//     }
  
-  };
-  render() {
-    // console.log(this.state.user)
-    let Component=this.handleComponent();
-    return (
-      <div >
-        {Component}
-      </div>
-    );
-  }
-}
+//   };
+//   render() {
+//     // console.log(this.state.user)
+//     let Component=this.handleComponent();
+//     return (
+//       <div >
+//         {Component}
+//       </div>
+//     );
+//   }
+// }
 
-export default HomePage;
+// export default HomePage;
 
 
 
@@ -198,193 +198,192 @@ export default HomePage;
 //   );
 // }
 
-// import { withStyles, makeStyles } from '@material-ui/core/styles';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-// import StyledTableCell from '@material-ui/core/TableCell';
-// import TableHead from '@material-ui/core/TableHead';
-// import TableRow from '@material-ui/core/TableRow';
-// import Paper from '@material-ui/core/Paper';
-// import Grid from '@material-ui/core/Grid';
-// import Button from '@material-ui/core/Button';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import 'material-design-icons/iconfont/material-icons.css';
-// import InputBase from '@material-ui/core/InputBase';
-// import React, { Component } from 'react';
-// import "./../../components/list_schedule_toeic/stylestoeic.css";
-// import 'material-design-icons/iconfont/material-icons.css';
-// import _ from "lodash";
-// class List_toiec extends Component {
-//   constructor(props){
-//     super(props);
-//     this.state={
-//       data:[
-//         {
-//           name:"Speaking",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"Write",
-//           data:"12/3/2019",
-//           local:"DH Qui Nhon",
-//           action:""
-//         },
-//         {
-//           name:"Listening",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"adc",
-//           data:"12/3/2019",
-//           local:"DH Qui Nhon",
-//           action:""
-//         },
-//         {
-//           name:"toiec 5",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"toiec 6",
-//           data:"12/3/2019",
-//           local:"DH Qui Nhon",
-//           action:""
-//         },
-//         {
-//           name:"toiec 7",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"toiec 8",
-//           data:"12/3/2019",
-//           local:"DH Qui Nhon",
-//           action:""
-//         },
-//         {
-//           name:"toiec 9",
-//           data:"12/3/2019",
-//           local:"DH Qui Nhon",
-//           action:""
-//         },
-//         {
-//           name:"toiec 10",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"tuoihahaa",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"hihihi",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"working",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"todoin",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//         {
-//           name:"hoaga",
-//           data:"12/3/2019",
-//           local:"75 Mai Xuan Thuong",
-//           action:""
-//         },
-//       ],
-//       search:"",
-//     }
-//   }
-//   handleChange= (event) => {
-//     this.setState({
-//       search:event.target.value
-//     }) 
-//   }
-  
-//   render() {
-//     const result=this.state.data;
-//     var search=this.state.search;
-//     console.log(search)
-//     var data= _.filter(result,(item)=>{
-//       return _.includes(item.name.toLowerCase(),search);
-//     })
-//     return (
-//       <div className="x">
-//       <Paper className="root">
-//         <Toolbar >
-//                     <Typography id="tableTitle">
-//                         <span className="title">TOEIC SCHEDULES</span>
-//                     </Typography>
-//                     <div className="spacer" />
-//                     <Paper className="search">
-//                         <InputBase
-//                             placeholder="Search toiec"
-//                             className="inputSearch"
-//                             onChange={(event) => this.handleChange(event)}
-//                              />
-//                         <i className="fa fa-search icon" aria-hidden="true" ></i>
-//                     </Paper>
-//                     <button type="button" className="btn buttonView">ADD
-//                     </button>
-//                 </Toolbar>
-//         <Table className="table "  >
-//         <TableHead className="color" >
-//           <TableRow >
-//             <StyledTableCell className="color1 columnName" > <b>Name Schedule</b> </StyledTableCell>
-//             <StyledTableCell className="color1" align="center"><b>Date</b></StyledTableCell>
-//             <StyledTableCell className="color1" align="center"><b>Location</b></StyledTableCell>
-//             <StyledTableCell className="color1" align="center"><b>Action</b></StyledTableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//             {
-//               data.map((value,key)=>{
-//                 return(
-//                   <TableRow key={key} className="tableRow">
-//                   <StyledTableCell component="th" scope="row">
-//                    {value.name}
-//                   </StyledTableCell>
-//                   <StyledTableCell align="center">{value.data} </StyledTableCell>
-//                   <StyledTableCell align="center">{value.local} </StyledTableCell>
-//                   <StyledTableCell align="center" >
-//                   <Button variant="contained" color="primary" className="a"  >
-//                   <i className="material-icons md-18 x"  >
-//                     edit</i>
-//                     Edit</Button>
-//                   <Button  variant="contained" color="default" className="a"  >
-//                   <i className="material-icons md-18 x" >
-//                    delete</i>
-//                     Delete</Button>
-//                   </StyledTableCell>
-//                 </TableRow>
-//                 )
-//               })
-//             }
-//         </TableBody>
-//       </Table>
-//       </Paper>
-//       </div>
-//     );
-//   }
-// }
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import StyledTableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import 'material-design-icons/iconfont/material-icons.css';
+import InputBase from '@material-ui/core/InputBase';
+import React, { Component } from 'react';
+import "./../../components/list_schedule_toeic/stylestoeic.css";
+import 'material-design-icons/iconfont/material-icons.css';
+import _ from "lodash";
+class List_toiec extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:[
+        {
+          name:"Speaking",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"Write",
+          data:"12/3/2019",
+          local:"DH Qui Nhon",
+          action:""
+        },
+        {
+          name:"Listening",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"adc",
+          data:"12/3/2019",
+          local:"DH Qui Nhon",
+          action:""
+        },
+        {
+          name:"toiec 5",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"toiec 6",
+          data:"12/3/2019",
+          local:"DH Qui Nhon",
+          action:""
+        },
+        {
+          name:"toiec 7",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"toiec 8",
+          data:"12/3/2019",
+          local:"DH Qui Nhon",
+          action:""
+        },
+        {
+          name:"toiec 9",
+          data:"12/3/2019",
+          local:"DH Qui Nhon",
+          action:""
+        },
+        {
+          name:"toiec 10",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"tuoihahaa",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"hihihi",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"working",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"todoin",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+        {
+          name:"hoaga",
+          data:"12/3/2019",
+          local:"75 Mai Xuan Thuong",
+          action:""
+        },
+      ],
+      search:"",
+    }
+  }
+  handleChange= (event) => {
+    this.setState({
+      search:event.target.value
+    }) 
+  }
+  render() {
+    const result=this.state.data;
+    var search=this.state.search;
+    console.log(search)
+    var data= _.filter(result,(item)=>{
+      return _.includes(item.name.toLowerCase(),search);
+    })
+    return (
+      <div className="x">
+      <Paper className="root">
+        <Toolbar >
+                    <Typography id="tableTitle">
+                        <span className="title">TOEIC SCHEDULES</span>
+                    </Typography>
+                    <div className="spacer" />
+                    <Paper className="search">
+                        <InputBase
+                            placeholder="Search toiec"
+                            className="inputSearch"
+                            onChange={(event) => this.handleChange(event)}
+                             />
+                        <i className="fa fa-search icon" aria-hidden="true" ></i>
+                    </Paper>
+                    <button type="button" className="btn buttonView">ADD
+                    </button>
+                </Toolbar>
+        <Table className="table "  >
+        <TableHead className="color" >
+          <TableRow >
+            <StyledTableCell className="color1 columnName" > <b>Name Schedule</b> </StyledTableCell>
+            <StyledTableCell className="color1" align="center"><b>Date</b></StyledTableCell>
+            <StyledTableCell className="color1" align="center"><b>Location</b></StyledTableCell>
+            <StyledTableCell className="color1" align="center"><b>Action</b></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {
+              data.map((value,key)=>{
+                return(
+                  <TableRow key={key} className="tableRow">
+                  <StyledTableCell component="th" scope="row">
+                   {value.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{value.data} </StyledTableCell>
+                  <StyledTableCell align="center">{value.local} </StyledTableCell>
+                  <StyledTableCell align="center" >
+                  <Button variant="contained" color="primary" className="a"  >
+                  <i className="material-icons md-18 x"  >
+                    edit</i>
+                    Edit</Button>
+                  <Button  variant="contained" color="default" className="a" >
+                  <i className="material-icons md-18 x" >
+                   delete</i>
+                    Delete</Button>
+                  </StyledTableCell>
+                </TableRow>
+                )
+              })
+            }
+        </TableBody>
+      </Table>
+      </Paper>
+      </div>
+    );
+  }
+}
 
-// export default List_toiec;
+export default List_toiec;
