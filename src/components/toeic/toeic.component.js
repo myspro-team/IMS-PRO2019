29a7608848d267291f1c1bddc1a7551b3d00f04a
PhanHoai <pthoai-batch2@sdc.edu.vn>
2019-09-09 16:24:25 +0700

    toeic_page

--- src/components/toeic/toeic.component.js
+++ src/components/toeic/toeic.component.js
@@ -6,55 +6,137 @@ import Paper from '@material-ui/core/Paper';
 import 'material-design-icons/iconfont/material-icons.css';
 import React, { Component } from 'react';
 import 'material-design-icons/iconfont/material-icons.css';
-import _ from "lodash";
+import { filter, includes, orderBy as OrderBy } from "lodash";
 import "./styles.css";
 import ToolbarTable from './toolbar';
 import TableHeader from './table/TableHead';
 import ButtonAction from './table/ButtonAction';
+import Pagination from './table/Pagination';
+import LoadingToeic from './table/LoadingToeic';
+// import orderBy from "lodash/orderBy"
 class ToeicTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            columns: [
+                { label: "Name_Schadule" },
+                { label: "Data" },
+                { label: "Location" },
+
+            ],
+            searchInfor: "",
             search: "",
+            currentPage: 0,
+            rowPerPage: 50,
+            order: "desc",
+            orderBy: "",
+            typeOrder: {
+                asc: "desc",
+                desc: "asc"
+            }
         }
     }
+
+    setCurrentPage = (page) => {
+        this.setState({
+            currentPage: page
+        })
+    }
+
+    setRowPerPage = (row) => {
+        this.setState({
+            rowPerPage: row
+        })
+    }
+
     handleSearch = (info) => {
         console.log(info);
         this.setState({
             search: info
         })
     }
-    render() {
-        const result = this.props.data;
-        var search = this.state.search;
-        var data = _.filter(result, (item) => {
-            return _.includes(item.name.toLowerCase(), search);
+
+    handleSort = (event, columnName) => {
+        console.log(columnName)
+        this.setState({
+            orderBy: columnName,
+            order: this.state.orderBy === columnName ? this.state.typeOrder[this.state.order] : 'asc'
         })
-        return (
-            <div className="x">
-                <Paper className="root">
-                    <ToolbarTable  Search={(info) => this.handleSearch(info)} />
-                    <div className="tableWrapper">
-                       <Table className="table">
-                       <TableHeader />
+        console.log(this.state)
+    }
+
+    displayTable() {
+        let result = this.props.data;
+        console.log(this.props.data)
+        var search = this.state.search;
+        var data = filter(result, (item) => {
+            return includes(item.Name_Schadule.toLowerCase(), search.toLowerCase());
+        })
+        let datasort = OrderBy(data, [this.state.orderBy], [this.state.order])
+        let indexOfLast = (this.state.currentPage + 1) * this.state.rowPerPage
+        let indexOfFirst = indexOfLast - this.state.rowPerPage
+        console.log(this.state.orderBy)
+        console.log(this.state.order)
+        let ListToeic = datasort.slice(indexOfFirst, indexOfLast);
+        console.log(ListToeic)
+
+        if (this.props.loading === true) {
+            return (
+                <LoadingToeic></LoadingToeic>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <Table className="table">
+                        <TableHeader data={this.state.columns}
+                            handleSort={(event, columnName) => this.handleSort(event, columnName)}
+                            order={this.state.order}
+                            orderBy={this.state.orderBy}
+
+                        />
                         <TableBody >
                             {
-                                data.map((value, index) => {
+                                ListToeic.map((value, index) => {
                                     return (
-                                        <TableRow  key={index} className="tableRow ">
-                                            <StyledTableCell component="th" scope="row">
-                                                {value.name}
+                                        <TableRow key={index} className="tableRow ">
+                                            <StyledTableCell align="left" >
+                                                {value.Name_Schadule}
                                             </StyledTableCell>
-                                            <StyledTableCell align="center">{value.data} </StyledTableCell>
-                                            <StyledTableCell align="center">{value.local} </StyledTableCell>
+                                            <StyledTableCell align="left">{value.Data} </StyledTableCell>
+                                            <StyledTableCell align="left">{value.Location} </StyledTableCell>
                                             <ButtonAction />
                                         </TableRow>
                                     )
                                 })
                             }
                         </TableBody>
-                       </Table>
-                    </div>                       
+                    </Table>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        let result = this.props.data;
+        // console.log(this.props.data)
+        var search = this.state.search;
+        var data = filter(result, (item) => {
+            return includes(item.Name_Schadule.toLowerCase(), search);
+        })
+        return (
+            <div className="x">
+                <Paper className="root">
+                    <ToolbarTable Search={(info) => this.handleSearch(info)} />
+                    <div className="tableWrapper height">
+                        {this.displayTable()}
+                    </div>
+                    <Pagination count={data.length}
+                        rowsPerPage={this.state.rowPerPage}
+                        page={this.state.currentPage}
+                        setCurrentPage={(page) => this.setCurrentPage(page)}
+                        setRowPerPage={(row) => this.setRowPerPage(row)}
+                    />
                 </Paper>
             </div>
         );
