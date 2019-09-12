9f253b55f5d054298704d7f20b73d3809a234560
nhanhaloli <thanhnhanpro.pro@gmail.com>
2019-09-12 10:50:47 +0700

    switch branch

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from "./containers/Login/Home.container";
+import Home from "./containers/Login/HomePage.container";
 import InternshipPage from './containers/internship/Internship.container'
 import AddInternship from './containers/internship/AddInternship.container'
 import ViewInternship from './containers/internship/ViewInternship.container'
@@ -12,7 +12,7 @@ export default (
       <Route path='/internship' component={InternshipPage} />
       <Route path='/addInternship' component={AddInternship} />
       <Route path='/detail.:id' component={ViewInternship} />
-      <Route path='/toeic' component={Toeic} />
+      {<Route path='/toeic' component={Toeic} />}
     </Route>
 
   </Route>
